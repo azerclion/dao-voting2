f31ba996cd0b483bf554934d6d190fd39c95e7f4
azerc <zizimoos@likelion.org>
2023-02-06 23:25:12 +0900

    ganache & goerli deploy, abi

--- README2.md
+++ README2.md
@@ -4,22 +4,20 @@ marp: true
 
 # Getting Started with Create React App
 
-괜히 잘난척 하는 걸로 오해가 될 것 같아서 ~
-
-도와주시는 분들도 없으시고 ~
-
-그래서 요청이 없으시면 하지 않는 것으로 ~
-
 ---
 
 ### 12월 7일 화요일
 
 1. 한꺼번에 return값 두개 확인하기 ✔️
-2. 테스트 안한 것들...뭐있지?
-3. migrations
-4. deploy (ganache)
-5. 지갑연결
-6. web3js
+2. deploy (ganache) ✔️
+3. 지갑연결
+4. migrations ✔️
+
+```
+   ❯ truffle migrate --network goerli --reset
+```
+
+5. web3js
 
 ---
 
@@ -27,6 +25,55 @@ marp: true
 
 1. styled-components
 2. deploy (goerli net)
+
+---
+
+```
+❯ truffle migrate --network goerli --reset
+
+Compiling your contracts...
+===========================
+> Everything is up to date, there is nothing to compile.
+
+
+Starting migrations...
+======================
+> Network name:    'goerli'
+> Network id:      5
+> Block gas limit: 30000000 (0x1c9c380)
+
+
+1_voting_migrations.js
+======================
+
+   Deploying 'B2'
+   --------------
+   > transaction hash:    0xdf45373f61abbd1f97c53a9e180628af9ea5a77dbccf203155cde53197a2dd1f
+   > Blocks: 1            Seconds: 17
+   > contract address:    0x1AB52eA150232A070C9E1DC3a6F7DCB5d229a495
+   > block number:        8444818
+   > block timestamp:     1675692072
+   > account:             0x56C53049a267d05578163706C3589D44061AD9A6
+   > balance:             4.996947994519038492
+   > gas used:            1217036 (0x12920c)
+   > gas price:           2.507736403 gwei
+   > value sent:          0 ETH
+   > total cost:          0.003052005480961508 ETH
+
+   Pausing for 2 confirmations...
+
+   -------------------------------
+   > confirmation number: 1 (block: 8444819)
+   > confirmation number: 2 (block: 8444820)
+   > Saving artifacts
+   -------------------------------------
+   > Total cost:     0.003052005480961508 ETH
+
+Summary
+=======
+> Total deployments:   1
+> Final cost:          0.003052005480961508 ETH
+```
 
 ---
 
--- truffle-config.js
+++ truffle-config.js
@@ -7,7 +7,7 @@
  * More information about configuration can be found at:
  *
  * https://trufflesuite.com/docs/truffle/reference/configuration
- * 
+ *
  * Hands-off deployment with Infura
  * --------------------------------
  *
@@ -15,36 +15,36 @@
  * Use this appproach to make deployment a breeze 🏖️:
  *
  * Infura deployment needs a wallet provider (like @truffle/hdwallet-provider)
- * to sign transactions before they're sent to a remote public node. 
+ * to sign transactions before they're sent to a remote public node.
  * Infura accounts are available for free at 🔍: https://infura.io/register
  *
  * You'll need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. You can store your secrets 🤐 in a .env file. 
- * In your project root, run `$ npm install dotenv`. 
- * Create .env (which should be .gitignored) and declare your MNEMONIC 
+ * public/private key pairs. You can store your secrets 🤐 in a .env file.
+ * In your project root, run `$ npm install dotenv`.
+ * Create .env (which should be .gitignored) and declare your MNEMONIC
  * and Infura PROJECT_ID variables inside.
  * For example, your .env file will have the following structure:
- * 
+ *
  * MNEMONIC = <Your 12 phrase mnemonic>
  * PROJECT_ID = <Your Infura project id>
- * 
+ *
  * Deployment with Truffle Dashboard (Recommended for best security practice)
  * --------------------------------------------------------------------------
- * 
+ *
  * Are you concerned about security and minimizing rekt status 🤔?
  * Use this method for best security:
- * 
- * Truffle Dashboard lets you review transactions in detail, and leverages 
- * MetaMask for signing, so there's no need to copy-paste your mnemonic. 
- * More details can be found at 🔎: 
- * 
+ *
+ * Truffle Dashboard lets you review transactions in detail, and leverages
+ * MetaMask for signing, so there's no need to copy-paste your mnemonic.
+ * More details can be found at 🔎:
+ *
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   /**
@@ -64,11 +64,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -82,13 +82,17 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${PROJECT_ID}`
+        ),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
@@ -106,7 +110,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.16",      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.16", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -115,7 +119,7 @@ module.exports = {
       //  },
       //  evmVersion: "byzantium"
       // }
-    }
+    },
   },
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
